862f8ad6c6fc040e683eaf665e2194802b139f0b
eshginfarzali <git>
2023-09-10 21:40:50 +0400

    fix -errors map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -2,62 +2,47 @@ import "./map.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
-import Location from'../../assets/location.png'
+import Location from "../../assets/location.png";
 
-import { Icon, divIcon, point } from "leaflet";
+import { Icon, LatLngTuple } from "leaflet";
 
-// create custom icon
+
+
 const customIcon = new Icon({
   iconUrl: Location,
-  iconSize: [38, 38] // size of the icon
+  iconSize: [38, 38],
 });
 
-// custom cluster icon
-const createClusterCustomIcon = function (cluster) {
-  return new divIcon({
-    html: `<span class="cluster-icon">${cluster.getChildCount()}</span>`,
-    className: "custom-marker-cluster",
-    iconSize: point(33, 33, true)
-  });
-};
-
-// markers
 const markers = [
   {
-    geocode: [40.4093, 49,86],
-    popUp: "Hello, Baku"
+    geocode: [51.05, -0.1250] as LatLngTuple,
+    popUp: "Hello, London",
   },
   {
-    geocode:[51.05, -0.1250],
-    popUp: "Hello, London"
+    geocode: [40.4093, 49.86] as LatLngTuple,
+    popUp: "Hello, Baku",
   },
   {
-    geocode: [37.000, -95.00],
-    popUp: "Branch 3"
-  }
+    geocode: [37.0, -95.0] as LatLngTuple,
+    popUp: "Branch 3",
+  },
 ];
 
 export function MapComponent() {
+  
   return (
     <MapContainer center={[48.5793, 23.2671]} zoom={2.4}>
- 
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
 
-
-      <MarkerClusterGroup
-        chunkedLoading
-        iconCreateFunction={createClusterCustomIcon}
-      >
-        {markers.map((marker) => (
-          <Marker position={marker.geocode} icon={customIcon}>
+      <MarkerClusterGroup chunkedLoading>
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.geocode} icon={customIcon}>
             <Popup>{marker.popUp}</Popup>
           </Marker>
         ))}
-
-     
       </MarkerClusterGroup>
     </MapContainer>
   );
